7a661c9b2b3c944971fdac7f8e09ebf12eb2429b
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-07-11 19:32:51 +0200

    initial commit

---
+++ src/main/java/cloud/cholewa/rective/BykowskiReactiveApplication.java
@@ -0,0 +1,14 @@
+package cloud.cholewa.rective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BykowskiReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BykowskiReactiveApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/cloud/cholewa/rective/BykowskiReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.rective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BykowskiReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
